3c4d133994b16086d71502cc59485db408e5ef06
Maxime <final7765@gmail.com>
2013-12-08 12:59:22 -0500

    Initial Commit

---
+++ UVweb/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UVweb
+//
+//  Created by Maxime on 08/12/2013.
+//  Copyright (c) 2013 UVweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
